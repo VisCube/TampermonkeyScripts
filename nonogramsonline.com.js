11109df558d551aaa25aef5e0f8fd05ce8fb968b
VisCube <180091136+VisCube@users.noreply.github.com>
2025-03-21 00:43:33 +0500

    feat(nonogramsonline.com): make cells square

---
+++ nonogramsonline.com.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         nonogramsonline.com
+// @namespace    https://github.com/VisCube/TampermonkeyScripts
+// @version      2025-03-21
+// @description  Stylistic changes for the puzzle page
+// @author       VisCube
+// @match        https://nonogramsonline.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=nonogramsonline.com
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    GM_addStyle(`
+        td.left-numbers {
+            white-space: nowrap;
+        }
+        td.puzzle-cell {
+            height: 50px;
+            min-width: 50px;
+        }
+    `);
+
+})();
